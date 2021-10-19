6e07807e161e165b1f6fb6692e2e4847337d9075
sheeba hasan <sheeba hasan@Dellpc>
2021-10-19 13:15:19 +0530

    crm project push

---
+++ CRM_Maven_Framework/src/test/java/com/crm/contacttest/SampleTest.java
@@ -0,0 +1,12 @@
+package com.crm.contacttest;
+
+import org.testng.annotations.Test;
+
+public class SampleTest {
+	@Test
+	public void sampleTest() {
+		String URL=System.getProperty("url");
+		System.out.println(URL);
+	}
+
+}
